b89ca6a59be4661f9e1c4c7b01641381dd6d6114
yaswanthkumaranbu <yaswanthkumaranbu.gmail.com>
2023-12-08 16:40:18 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useRef, useState} from 'react';
+import {
+  Button,
+  Text,
+  StyleSheet,
+  View,
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const Home = () => {
+  const drawer = useRef(null);
+  const [drawerPosition, setDrawerPosition] = useState('left');
+//   const changeDrawerPosition = () => {
+//     if (drawerPosition === 'left') {
+//       setDrawerPosition('right');
+//     } else {
+//       setDrawerPosition('left');
+//     }
+//   };
+
+  const navigationView = () => (
+    <View style={[styles.container, styles.navigationContainer]}>
+      <Button
+        title="Close drawer"
+        onPress={() => drawer.current.closeDrawer()}
+      />
     </View>
   );
-}
+
+  return (
+    <NavigationContainer
+      ref={drawer}
+      drawerWidth={300}
+      drawerPosition={drawerPosition}
+      renderNavigationView={navigationView}>
+      <View style={styles.container}>
+        <Button
+          title="Open drawer"
+          onPress={() => drawer.current.openDrawer()}
+        />
+      </View>
+    </NavigationContainer>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 16,
+  },
+  navigationContainer: {
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    padding: 16,
+    fontSize: 15,
+    textAlign: 'center',
   },
 });
+
+export default Home;
---
+++ components/Home.js
@@ -0,0 +1,24 @@
+import React, {useRef, useState} from 'react';
+import {
+  Button,
+  DrawerLayoutAndroid,
+  Text,
+  StyleSheet,
+  View,
+} from 'react-native';
+
+const Home = () => {
+
+
+
+
+  return (<><Text>I'm in sidebar</Text></>
+
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default Home;
